e16b2ab9e8dc581caeae5ce9bf4e0f24b2913712
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2023-06-11 22:14:12 -0500

    ups

--- src/persons/persons.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PersonsController } from './persons.controller';
-import { PersonsService } from './persons.service';
-
-@Module({
-  controllers: [PersonsController],
-  providers: [PersonsService]
-})
-export class PersonsModule {}
